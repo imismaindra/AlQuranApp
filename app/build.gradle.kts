c1c30e336f810818b7e6f25d6aebaa401741b226
Ismaindra <imismaindra@gmail.com>
2025-11-18 09:47:33 +0700

    change icon launcher

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/ismaindra/alquranapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/ismaindra/alquranapp/ui/main/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.ismaindra.alquranapp.databinding.ActivityMainBinding
 import com.ismaindra.alquranapp.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
